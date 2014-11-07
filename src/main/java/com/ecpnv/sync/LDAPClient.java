fdd78747a902dfa7af8955860cf5b7f3105cfa97
Sander Ginn <sanderdeginno@gmail.com>
2014-11-07 09:37:47 +0100

    Update to JUnit4

--- src/main/java/com/ecpnv/sync/LDAPClient.java
+++ src/main/java/com/ecpnv/sync/LDAPClient.java
@@ -1,7 +1,5 @@
 package com.ecpnv.sync;
 
-import static org.junit.Assert.assertNotNull;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,42 +14,41 @@ import org.apache.directory.ldap.client.api.LdapNetworkConnection;
 
 public class LDAPClient {
 
-	private static final String BASE = "OU=Phonebook,DC=3cx,DC=ECP";
-	private LdapConnection connection;
+    private static final String BASE = "OU=Phonebook,DC=3cx,DC=ECP";
+    private LdapConnection connection;
 
-	public LDAPClient(String host) throws LdapException {
-		connection = new LdapNetworkConnection(host, 389);
-		connection.bind();
-	}
+    public LDAPClient(String host) throws LdapException {
+        connection = new LdapNetworkConnection(host, 389);
+        connection.bind();
+    }
 
-	public List<String> search(String x) throws CursorException, LdapException {
+    public List<String> search(String x) throws CursorException, LdapException {
 
-		List<String> results = new ArrayList<String>();
+        List<String> results = new ArrayList<String>();
 
-		EntryCursor cursor = connection.search(BASE, "(objectClass=person)",
-				SearchScope.SUBTREE);
+        EntryCursor cursor = connection.search(BASE, "(objectClass=person)",
+                SearchScope.SUBTREE);
 
-		while (cursor.next()) {
-			Entry entry = cursor.get();
-			assertNotNull(entry);
-			System.out.println(entry);
-			results.add(entry.toString());
-		}
+        while (cursor.next()) {
+            Entry entry = cursor.get();
+            System.out.println(entry);
+            results.add(entry.toString());
+        }
 
-		cursor.close();
+        cursor.close();
 
-		return results;
+        return results;
 
-	}
+    }
 
-	public void add(String name, String phoneNumber) throws LdapException {
-		connection.add(new DefaultEntry(
-				"cn="+name+"," + BASE, 
-				"ObjectClass: top",
-				"ObjectClass: person", 
-				"cn: "+name, 
-				"telephoneNumber: "+phoneNumber
-				));
-	}
+    public void add(String name, String phoneNumber) throws LdapException {
+        connection.add(new DefaultEntry(
+                "cn=" + name + "," + BASE,
+                "ObjectClass: top",
+                "ObjectClass: person",
+                "cn: " + name,
+                "telephoneNumber: " + phoneNumber
+                ));
+    }
 
 }
--- src/test/java/com/ecpnv/sync/LDAPClientTest.java
+++ src/test/java/com/ecpnv/sync/LDAPClientTest.java
@@ -9,19 +9,19 @@ import org.junit.Test;
 
 public class LDAPClientTest {
 
-	@Test
-	public void test() throws Exception {
+    @Test
+    public void test() throws Exception {
 
-		LDAPClient client = new LDAPClient("10.3.0.201");
-		List<String> search = client.search(null);
-		assertThat(search.toString(), search.size(), is(2));
+        LDAPClient client = new LDAPClient("10.3.0.201");
+        List<String> search = client.search(null);
+        assertThat(search.toString(), search.size(), is(2));
 
-	}
+    }
 
-	@Test
-	public void testAdd() throws Exception {
-		LDAPClient client = new LDAPClient("10.3.0.201");
-		client.add("Marc Alvares", "+31 655 12312");
-	}
+    @Test
+    public void testAdd() throws Exception {
+        LDAPClient client = new LDAPClient("10.3.0.201");
+        client.add("Marc Alvares", "+31 655 12312");
+    }
 
 }
