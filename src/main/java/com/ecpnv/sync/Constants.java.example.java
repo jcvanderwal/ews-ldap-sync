e816dfeaf286734f1d6b986baa1203665f722eb0
Sander Ginn <sanderdeginno@gmail.com>
2014-11-12 10:39:16 +0100

    finally

--- src/main/java/com/ecpnv/sync/Constants.java.example.java
+++
@@ -1,7 +0,0 @@
-package com.ecpnv.sync;
-
-public class Constants {
-    public static final String USERNAME = "";
-    public static final String PASSWORD = "";
-    public static final String EWS_URI = "";
-}
