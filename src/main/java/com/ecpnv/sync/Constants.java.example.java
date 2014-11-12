6b9c793e04cd429686dd73dbfc37835a6a260a07
Sander Ginn <sanderdeginno@gmail.com>
2014-11-12 10:35:11 +0100

    Example constants file added

---
+++ src/main/java/com/ecpnv/sync/Constants.java.example.java
@@ -0,0 +1,7 @@
+package com.ecpnv.sync;
+
+public class Constants {
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+    public static final String EWS_URI = "";
+}
